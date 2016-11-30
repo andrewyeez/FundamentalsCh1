713a62e0c8761e527e751ddc80535ce4b6c9fb62
Andrew Yee <andrewyee18331@sbcglobal.net>
2016-11-29 21:18:37 -0800

    A8 & A9

--- src/ch1/Ch1.java
+++ src/ch1/Ch1.java
@@ -78,7 +78,6 @@ public class Ch1 {
 //        
 //        String line = "-------------------------------------------------------";
 //        System.out.println(line);
-        
         // A4
 //        int e = 0;
 //        int ee = e + 1;
@@ -89,7 +88,6 @@ public class Ch1 {
 //        System.out.println(" e + 1: " + ee);
 //        System.out.println("e += 1: " + ex);
 //        System.out.println("   e++: " + ey);
-        
         // A5
 //        Random rand = new Random();
 //        int row = 3; // 3 rows and ?? columns
@@ -109,23 +107,54 @@ public class Ch1 {
 //            for(int y = 0; y < arr[i].length; y++){
 //                System.out.println("arr["+i+"]["+y+"]: " + arr[i][y]);
 //            }
-//        }  
-        
+//        }
         // A6
-        
         // By default Java is always pass by value including the parameters
         // used in public static main! Pass by reference vs pass by value.
         // reference means you are handling the address/location of the actual
         // value while pass by value means you are given an exact copy of the
         // actual value.
-        
-        // A7
-        
+        // A8
         // We use void methods when we dont want to return anything with our 
         // method. A void static method by definition is just a method that
         // can be instantiated without creating an instance of the class and
         // does not return anything.
+        // A7
+        // Overloading is when you have the same method name but have different
+        // type/values in the parameters.
+//        Ch1 c = new Ch1();
+//        c.over_load("Andrew");
+//        c.over_load(1);
+
+        // A9
+        // Recursion is when a method return value is itself and it does this over
+        // and over, until a certain condition is met in the method that would
+        // remove itself from the recursive loop, Therefore, giving you the 
+        // desired outcome.
+//        Ch1 c = new Ch1();
+//        int num = c.recurse(10);
+//        System.out.println("This should be 2 = " + num);
         
     }
     
+    // A7
+//    public void over_load(int x){
+//        System.out.println("Type INT: " + x);
+//    }
+//    public void over_load(String x){
+//        System.out.println("Type STRING: " + x);
+//    }
+    // A9
+//    public int recurse(int x){
+//        if(x > 2){
+//            x--;
+//        }else if(x < 2){
+//            x++;
+//        }
+//        if(x != 2){
+//            System.out.println("recursing because 2 != " + x);
+//            return recurse(x);
+//        }
+//        return x;
+//    }
 }
